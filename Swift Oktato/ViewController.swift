36564b86fe9a2a8efe05bb407c1969623037ff68
Benke Smith <kovbenc@kovacss-macbook.local>
2018-02-26 19:11:32 +0100

    Initial Commit

---
+++ Swift Oktato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Oktato
+//
+//  Created by Kovács Bence on 2018. 02. 26..
+//  Copyright © 2018. Benke Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
