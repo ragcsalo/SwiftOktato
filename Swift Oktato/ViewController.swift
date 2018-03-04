8030aca27c90deb2a07b0795a1cf5aa29a817053
Kovács Bence <kovbenc@kovacss-macbook.local>
2018-03-04 19:27:04 +0100

    Első mentésünk a Git fejezetben. :-)

--- Swift Oktato/ViewController.swift
+++ Swift Oktato/ViewController.swift
@@ -10,9 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var gombSzamol = 0
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var btnLabel: UIButton!
+    
+    @IBAction func gombNyomas(_ sender: Any) {
+        gombSzamol += 1
+        print(gombSzamol)
+        if gombSzamol==5 {
+            view.backgroundColor = UIColor.yellow
+            myLabel.text = "Mental Painter"
+            btnLabel.backgroundColor = UIColor.cyan
+            btnLabel.setTitle("na még 5-ször", for: .normal)
+        }
+        else if gombSzamol==10 {
+            view.backgroundColor = UIColor.white
+            myLabel.text = "Benke Smith"
+            gombSzamol=0
+            btnLabel.backgroundColor = UIColor.white
+            btnLabel.setTitle("nyomj le 5-ször", for: .normal)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
